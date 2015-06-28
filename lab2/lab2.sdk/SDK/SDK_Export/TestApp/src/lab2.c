87964cea4be3e7a025c224dad32d78f284bff428
tangzt <tang_zhiting@163.com>
2015-06-28 16:09:18 +0800

    final lab

--- lab2/lab2.sdk/SDK/SDK_Export/TestApp/src/lab2.c
+++ lab2/lab2.sdk/SDK/SDK_Export/TestApp/src/lab2.c
@@ -22,9 +22,9 @@ int main (void)
 	while (1)
 	{
 	  psb_check = XGpio_DiscreteRead(&push, 1);
-	  xil_printf("Push Buttons Status %x\r\n", psb_check);
+	  xil_printf("336Push Buttons Status %x\r\n", psb_check);
 	  dip_check = XGpio_DiscreteRead(&dip, 1);
-	  xil_printf("DIP Switch Status %x\r\n", dip_check);
+	  xil_printf("336DIP Switch Status %x\r\n", dip_check);
 	  
 	  for (i=0; i<9999999; i++); 
 	}
